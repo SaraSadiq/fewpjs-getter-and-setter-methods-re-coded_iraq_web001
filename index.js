b30d7787ccbb9e4fe287154e922d6d263dd245ad
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-10 13:43:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ class Circle {
   constructor(radius) {
     this.radius = radius
   }
+  get diameter(){
+    return this.radius * 2
+  }
+
+  set diameter(){
+     this.radius / 2
+  }
+get circumference(){
+  return (this.radius * 2) * pi
+}
+set circumference(circumference){
+   (this.radius )=circumference/(pi*2)
+}  
 }
